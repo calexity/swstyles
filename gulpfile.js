ecb838ce8b8ea1e7e61d307478efdbb999350206
Alexa Roman <alexaroman@Alexas-MacBook-Pro.local>
2016-09-01 22:52:43 -0700

    set up for gh-pages deploy

--- gulpfile.js
+++ gulpfile.js
@@ -176,3 +176,14 @@ gulp.task('default', ['clean'], () => {
   });
 
 });
+
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
